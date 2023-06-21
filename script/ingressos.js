2140d26b3f4b1349002124bf1f51520757dfc605
Otávio <otaviomarcal785@gmail.com>
2023-06-21 00:58:17 -0300

    Página de ingressos

---
+++ script/ingressos.js
@@ -0,0 +1,16 @@
+const Quant = document.querySelectorAll('.quant')
+const BotaoMenos = document.querySelector('.botao-menos')
+const BotaoMais = document.querySelector('.botao-mais')
+
+let contador = 
+
+BotaoMenos.addEventListener('click', FunDiminui)
+BotaoMais.addEventListener('click', FunAumenta)
+
+function FunDiminui() {
+    if ( > 4) {
+        alert('Você escedeu o limite de ingressos')
+    } else {
+
+    }
+}
