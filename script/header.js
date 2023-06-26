fa5b4d0074d61e33967dc63d585e0da46f2b6541
Otávio <otaviomarcal785@gmail.com>
2023-06-26 14:27:39 -0300

    Correção pathname

--- script/header.js
+++ script/header.js
@@ -1,24 +1,22 @@
-const url = window.location.pathname
+const url = window.location.pathname.split('/').slice(-1)
 const aIndex = document.querySelector(".a-index")
 const aLine = document.querySelector(".a-line")
 const aArtistas = document.querySelector(".a-artistas")
 const aPlaylist = document.querySelector(".a-playlist")
 const aIngressos = document.querySelector(".a-ingressos")
 
-console.log(aIndex)
-
-if(url == "/index.html"){
+if(url == "index.html"){
     aIndex.classList.add("active")
 } 
-else if(url == "/line.html"){
+else if(url == "line.html"){
     aLine.classList.add("active")
 }
-else if(url == "/artistas.html"){
+else if(url == "artistas.html"){
     aArtistas.classList.add("active")
 }
-else if(url == "/playlist.html"){
+else if(url == "playlist.html"){
     aPlaylist.classList.add("active")
 }
-else if(url == "/ingressos.html"){
+else if(url == "ingressos.html"){
     aIngressos.classList.add("active")
 }
