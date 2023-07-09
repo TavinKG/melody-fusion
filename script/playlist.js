ae5a4665ba366d0a0b7da3001ac70e458b62147c
Otávio <otaviomarcal785@gmail.com>
2023-07-08 23:32:30 -0300

    function random bg color

--- script/playlist.js
+++ script/playlist.js
@@ -61,6 +61,27 @@ function loadTrack(trackIndex){
     random_bg_color();
 }
 
+function random_bg_color(){
+    let hex = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e'];
+    let a;
+
+    function populate(a){
+        for(let i=0; i<6; i++){
+            let x = Math.round(Math.random() * 14);
+            let y = hex[x];
+            a += y;
+        }
+        return a;
+    }
+    let Color1 = populate('#');
+    let Color2 = populate('#');
+    var angle = 'to right';
+
+    let gradient = 'linear-gradient(' + angle + ',' + Color1 + ', ' + Color2 + ")";
+    document.body.style.background = gradient; 
+}
+
+
 
 
 
